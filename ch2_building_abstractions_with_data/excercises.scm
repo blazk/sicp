13c74cf181cd8844f9cccb8b05ebb349726aee52
Blazej Krzeminski <bkrzem@gmail.com>
2016-01-06 01:28:18 +0000

    ex 2.33

--- ch2_building_abstractions_with_data/excercises.scm
+++ ch2_building_abstractions_with_data/excercises.scm
@@ -370,3 +370,42 @@
   (print (subsets s))
 )
 (excercise-2-32)
+
+
+
+
+(define (excercise-2-33)
+
+;; Exercise 2.33: Fill in the missing expressions to complete
+;; the following definitions of some basic list-manipulation
+;; operations as accumulations:
+
+;; first, lets define accumulate:
+
+  (define (accumulate op initial sequence)
+    (if (null? sequence)
+      initial
+      (op (car sequence) (accumulate op initial (cdr sequence)))))
+
+;; map as accumulation:
+
+  (define (map p sequence)
+    (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+;; append as accumulation:
+
+  (define (append seq1 seq2)
+    (accumulate cons seq2 seq1))
+
+;; length as accumulation
+
+  (define (length seq)
+    (accumulate (lambda (x y) (+ y 1)) 0 seq))
+
+;; test
+
+  (print (map (lambda (x) (* x x)) (list 1 2 3)))
+  (print (append (list 1 2) (list 3 4)))
+  (print (length (list 0 0 0 0 0)))
+)
+(excercise-2-33)
