88c34b46b11229829b95c54fd6af343ce7849336
Blazej Krzeminski <bkrzem@gmail.com>
2016-01-07 23:08:28 +0000

    2.35 count-leaves with map and accumulate

--- ch2_building_abstractions_with_data/excercises.scm
+++ ch2_building_abstractions_with_data/excercises.scm
@@ -435,3 +435,19 @@
 )
 (excercise-2-34)
 
+
+
+
+
+(define (excercise-2-35)
+
+;; Exercise 2.35: Redefine count-leaves from
+;; section 2.2.2 as an accumulation:
+
+  (define (count-leaves t)
+    (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1)) t)))
+
+  (print "2-35 count-leaves")
+  (print (count-leaves '(1 2 (3 4 ((5))))))
+)
+(excercise-2-35)
