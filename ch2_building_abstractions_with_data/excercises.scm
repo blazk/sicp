80f5b51ddfee780579ab722cdcd8535ac16d5a26
Blazej Krzeminski <bkrzem@gmail.com>
2016-01-08 00:44:47 +0000

    2.36: accumulate-n

--- ch2_building_abstractions_with_data/excercises.scm
+++ ch2_building_abstractions_with_data/excercises.scm
@@ -429,6 +429,8 @@
                 0
                 coefficients))
 
+;; test
+
   (define c (list 1 2 3))
   (print "2.34 horner-eval")
   (print (horner-eval 1 c))
@@ -439,15 +441,51 @@
 
 
 
+
 (define (excercise-2-35)
 
 ;; Exercise 2.35: Redefine count-leaves from
-;; section 2.2.2 as an accumulation:
+;; section 2.2.2 as an accumulation.
 
   (define (count-leaves t)
     (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1)) t)))
 
+;; test
+
   (print "2-35 count-leaves")
-  (print (count-leaves '(1 2 (3 4 ((5))))))
+  (print (count-leaves '(0 0 (0 0 ((0 0))))))     ;prints 6
 )
 (excercise-2-35)
+
+
+
+
+
+
+(define (excercise-2-36)
+
+;; Exercise 2.36: The procedure accumulate-n is similar to
+;; accumulate except that it takes as its third argument a
+;; sequence of sequences, which are all assumed to have the
+;; same number of elements. It applies the designated accu-
+;; mulation procedure to combine all the first elements of the
+;; sequences, all the second elements of the sequences, and so
+;; on, and returns a sequence of the results. For instance, if s
+;; is a sequence containing four sequences, ((1 2 3) (4 5 6)
+;; (7 8 9) (10 11 12)), then the value of (accumulate-n +
+;; 0 s) should be the sequence (22 26 30). Fill in the missing
+;; expressions in the following definition of accumulate-n:
+
+  (define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+;; test
+
+  (define x '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+  (print "2.36 accumulate-n")
+  (print (accumulate-n + 0 x))            ;prints (22 26 30)
+)
+(excercise-2-36)
